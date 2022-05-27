ed814bfb322055825c9aa22bf1af8bf17af04862
iliamamageishvili <106377290+iliamamageishvili@users.noreply.github.com>
2022-05-27 03:00:04 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,21 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Rectangle rec1 = new Rectangle();
-		Rectangle rec2 = new Rectangle();
-		Rectangle rec3 = null;
-//	System.out.println(rec3.area()); აბრუნებს nullpointexceptions;
-		
-//	System.out.println(rec1.getHeight()); აბრუნებს 0.0ს ფლოატის შემთხვევაში;
-		
-	System.out.println(rec1 == rec2); // აბრუნებს flase რადგან ობიექტები მეხსიერების სხვადასხვა ადგილებში არიან;
-	rec1=rec2;
-	System.out.println(rec1 == rec2); // ამ შემთხვევაში აბრუნებს trues რადგან ობიექტები მხსიერების ერთ ადგილზე არიან;
-	rec1.setHeight(12);	
-	
-	System.out.println(rec2.getHeight());
-// როგორც ვიცით თავიდან rec2 ის სიმაღლე იყო 0,0 და როდესაც rec1 ის სიმაღლე გახდა 12 შესაბამისად rec2 ის სიმაღლეც გახდა 12;	
-		
+		int a;
+		// int x = a+1; ინიციალიზაციის გარეშე მოქმედებებს ვერ ჩავატარებთ
+		int b=2;
+	if(b ==2) {
+		int c =1;
+	}
+	if(b != 2) {
+		int c =1;
+	}
 	}
 
 }
